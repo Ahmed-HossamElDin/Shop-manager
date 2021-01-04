75525bb25483ec2712999b9e66f5c87b623eb436
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-01-04 21:00:47 +0200

    login page

--- snoopyshop/src/snoopyshop/view/Snoopyshop.java
+++ snoopyshop/src/snoopyshop/view/Snoopyshop.java
@@ -3,13 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package snoopyshop;
+package snoopyshop.view;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,24 +24,16 @@ import javafx.stage.Stage;
 public class Snoopyshop extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("loginpage.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 600 , 400);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
+        
+        primaryStage.setTitle("Login");        
+       
         primaryStage.show();
     }
 
