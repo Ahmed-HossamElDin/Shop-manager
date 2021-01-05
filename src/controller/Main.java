55efd489a4556465cba21c58c7e0d24693b75958
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-01-05 14:01:38 +0200

    ta3del tafeh

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/loginpage.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
