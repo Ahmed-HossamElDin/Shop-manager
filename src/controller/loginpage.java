6656c0fd76085f8cd74e4a2b7daf654c0d40398b
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-01-05 16:10:34 +0200

    login transitions(v1)

--- src/controller/loginpage.java
+++ src/controller/loginpage.java
@@ -4,9 +4,30 @@ import javafx.event.ActionEvent;
 
 public class loginpage {
     private Scenes scenes = new Scenes();
-
-    public void login(ActionEvent actionEvent) throws Exception {
+    private Boolean isAdmin = true;
+    
+	public void setIsNotAdmin() {
+		this.isAdmin = false;
+	}
+	public void setIsAdmin() {
+		this.isAdmin = true;
+	}
+	 
+    public void AdminLogin(ActionEvent actionEvent) throws Exception {
         scenes.transition(actionEvent, "/view/admin_managment.fxml", "controller.loginpage");
+    }
+    public void StaffMemberLogin(ActionEvent actionEvent) throws Exception {
+        scenes.transition(actionEvent, "/view/cart.fxml", "controller.loginpage");
+    }
+    public void Login(ActionEvent actionEvent) throws Exception {
+        if(isAdmin == true)
+        {
+        	AdminLogin(actionEvent);
+        }
+        else
+        {
+        	StaffMemberLogin(actionEvent);
+        }  	
     }
 
 }
