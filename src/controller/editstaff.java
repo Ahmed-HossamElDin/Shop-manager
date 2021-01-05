8ae79aba35e707c0d665e0c35238059d300cff92
AhmedBahieldien <67127051+AhmedBahieldien@users.noreply.github.com>
2021-01-05 22:07:06 +0200

    5od

--- src/controller/editstaff.java
+++ src/controller/editstaff.java
@@ -7,4 +7,9 @@ public class editstaff { private Scenes scenes = new Scenes();
         scenes.transition(actionEvent, "/view/admin_managment.fxml", "controller.editstaff","Admin Managment`");
     }
 
+
+
+
+
+
 }
