be4e6069768bee7ac70f33775190c37d80ffa151
AhmedBahieldien <67127051+AhmedBahieldien@users.noreply.github.com>
2021-01-06 00:25:26 +0200

    provider done

--- src/controller/addprovider.java
+++ src/controller/addprovider.java
@@ -1,10 +1,34 @@
 package controller;
 
+import Connectivity.ConnectionClass;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class addprovider {private Scenes scenes = new Scenes();
 
     public void backbtn(ActionEvent actionEvent) throws Exception{
         scenes.transition(actionEvent, "/view/admin_managment.fxml", "controller.creatingnewstaff","Admin Managment`");
+    }
+
+
+
+    @FXML
+    private TextField id, name, phone;
+
+
+    @FXML
+    public void done(ActionEvent actionEvent) throws SQLException {
+
+        ConnectionClass connectionClass=new ConnectionClass();
+        Connection connection=connectionClass.getConnection();
+        String sql = "INSERT INTO providers " + "VALUES ("+id.getText()+", '"+name.getText()+"', '"+phone.getText()+"')";
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sql);
+
     }
 }
--- src/controller/deleteprovider.java
+++ src/controller/deleteprovider.java
@@ -1,10 +1,28 @@
 package controller;
 
+import Connectivity.ConnectionClass;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class deleteprovider {private Scenes scenes = new Scenes();
 
     public void backbtn(ActionEvent actionEvent) throws Exception{
         scenes.transition(actionEvent, "/view/admin_managment.fxml", "controller.creatingnewstaff","Admin Managment`");
+    }
+
+    @FXML
+    private TextField id;
+
+    public void done(ActionEvent actionEvent) throws Exception {
+        ConnectionClass connectionClass=new ConnectionClass();
+        Connection connection=connectionClass.getConnection();
+        String sql = "DELETE FROM providers WHERE id = '"+id.getText()+"'";
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sql);
+        System.out.println("provider deleted");
     }
 }
